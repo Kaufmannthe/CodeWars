eb9d7759dd38d9fe62ad597e678ef62ef3a6448d
Artyom <kaufmannthe@gmail.com>
2022-06-09 21:32:12 +0300

    Add new task

--- src/com/codewars/kyu8/toUpperCase.java
+++ src/com/codewars/kyu8/toUpperCase.java
@@ -1,6 +1,9 @@
 package com.codewars.kyu8;
 
 public class toUpperCase {
+    public static void main(String[] args) {
+        System.out.println(MakeUpperCase("lowercase"));
+    }
 
     public static String MakeUpperCase(String str){
     return str.toUpperCase();
